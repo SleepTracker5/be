39dbbdb840633ac500983f82cf0d1b06118454f7
David White <dwhite0627@gmail.com>
2020-05-21 07:29:22 -0400

    Fix issue with knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
 module.exports = {
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL, ,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations",
     },
     seeds: {
       directory: "./data/seeds",
-    }
+    },
   },
   development: {
     client: "sqlite3",
