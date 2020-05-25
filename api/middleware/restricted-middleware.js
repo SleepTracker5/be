9f44d26f8ffed2e4d25160785c056deb5e5bab4b
David White <dwhite0627@gmail.com>
2020-05-25 17:06:01 -0400

    Add restrictByRole to the restricted middleware

--- api/middleware/restricted-middleware.js
+++ api/middleware/restricted-middleware.js
@@ -1,8 +1,13 @@
 const jwt = require("jsonwebtoken");
 const { errDetail } = require("../utils/utils");
 
-// constants
-const minSecurityLevel = 2;
+// Constants
+const authError = {
+  message: "Invalid credentials",
+  validation: [],
+  data: {},
+};
+const adminSecurityLevel = 2;
 
 async function decodeJWT(req, res, next) {
   // Objects
@@ -24,12 +29,6 @@ async function decodeJWT(req, res, next) {
 }
 
 function restrict() {
-  const authError = {
-    message: "Invalid credentials",
-    validation: [],
-    data: {},
-  };
-
   return async (req, res, next) => {
     try {
       const token = req.headers.authorization || req.cookies.token;
@@ -38,7 +37,7 @@ function restrict() {
       }
 
       jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err || decoded.role < minSecurityLevel) {
+        if (err) {
           return res.status(401).json(authError);
         }
         req.token = decoded;
@@ -50,4 +49,26 @@ function restrict() {
   };
 }
 
-module.exports = { restrict, decodeJWT };
+// Admin role permissions
+function restrictByRole() {
+  return async (req, res, next) => {
+    try {
+      const token = req.headers.authorization || req.cookies.token;
+      if (!token) {
+        return res.status(401).json(authError);
+      }
+
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err || decoded.role < adminSecurityLevel) {
+          return res.status(401).json(authError);
+        }
+        req.token = decoded;
+        next();
+      });
+    } catch (err) {
+      errDetail(res, err);
+    }
+  };
+}
+
+module.exports = { restrict, restrictByRole, decodeJWT };
