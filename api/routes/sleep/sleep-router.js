33a43e588b45a803678ae54b216d52038f0ecb82
David White <dwhite0627@gmail.com>
2020-05-26 20:59:13 -0400

    Add an update method to the sleep-router

--- api/routes/sleep/sleep-router.js
+++ api/routes/sleep/sleep-router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const moment = require("moment");
 
 // Db helpers, utils
-const { find, findBy, update, remove } = require("./sleep-model");
+const { find, findBy, insert, update, remove } = require("./sleep-model");
 const { errDetail, validateTime } = require("../../utils/utils");
 
 // Constants
@@ -54,6 +54,19 @@ router.get("/:id", async (req, res) => {
     const id = Number(req.params.id);
     const user_id = req.token.id;
     const sleep = await findBy({ id, user_id });
+    res.status(200).json({
+      message: "Success",
+      validation: [],
+      data: sleep,
+    });
+  } catch (err) {
+    errDetail(res, err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const sleep = await insert(req.body);
     res.status(200).json({
       message: "Success",
       validation: [],
