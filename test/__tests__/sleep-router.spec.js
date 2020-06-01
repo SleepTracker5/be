ce6873ece2476e633f4074101ea12e13e16e0954
David White <dwhite0627@gmail.com>
2020-06-01 00:11:33 -0400

    Add integration test of GET /sleep route, auth test components

--- test/__tests__/sleep-router.spec.js
+++ test/__tests__/sleep-router.spec.js
@@ -66,11 +66,11 @@ const testAdmin = {
 //   data: {},
 // };
 
-// const authError = {
-//   message: "Invalid credentials",
-//   validation: [],
-//   data: {},
-// };
+const authError = {
+  message: "Invalid credentials",
+  validation: [],
+  data: {},
+};
 
 // Test entry credentials
 const sleepEntry = {
@@ -80,6 +80,26 @@ const sleepEntry = {
   user_id: 1,
   mood_waking: 3,
   mood_day: 3,
+  mood_bedtime: 3,
+};
+
+const sleepEntry2 = {
+  sleep_start: "1585872000000",
+  sleep_end: "1585908000000",
+  sleep_goal: 6,
+  user_id: 1,
+  mood_waking: 4,
+  mood_day: 2,
+  mood_bedtime: 4,
+};
+
+const sleepEntry3 = {
+  sleep_start: "1586811600000",
+  sleep_end: "1586829600000",
+  user_id: 1,
+  sleep_goal: 8,
+  mood_waking: 4,
+  mood_day: 4,
   mood_bedtime: 3,
 };
 
@@ -137,8 +157,18 @@ describe("the sleep route", () => {
     }
   });
 
+  // Add a sleep entry
   it("inserts a new sleep entry", async done => {
     try {
+      // It should return a 401 if not logged in
+      // prettier-ignore
+      const noAuth = await request(server)
+        .post("/api/sleep")
+        .send(sleepEntry);
+      expect(noAuth.statusCode).toBe(401);
+      expect(noAuth.type).toBe("application/json");
+      expect(noAuth.body).toEqual(authError);
+
       // Attempt login
       // prettier-ignore
       const loginRes = await request(server)
@@ -173,6 +203,74 @@ describe("the sleep route", () => {
     }
   });
 
+  it("returns all sleep data for a user", async done => {
+    // Register a user so that the update can change the user_id to this user id
+    // prettier-ignore
+    const regRes = await request(server)
+      .post("/api/register")
+      .send(testAdmin);
+    expect(regRes.statusCode).toBe(201);
+
+    // Attempt login
+    // prettier-ignore
+    const loginRes = await request(server)
+      .post("/api/login")
+      .send(userLogin);
+    expect(loginRes.statusCode).toBe(200);
+    expect(loginRes.type).toBe("application/json");
+    expect(loginRes.headers["set-cookie"]).toBeDefined(); // token cookie
+    expect(loginRes.body.data.user.username).toBe(userLogin.username);
+    const token = loginRes.body.data.token;
+    expect(token).toBeDefined();
+    const validJWT = await isVerifiedJWT(token);
+    expect(validJWT).toBe(true);
+
+    // Add the first entry
+    const res1 = await request(server)
+      .post("/api/sleep")
+      .set({ authorization: token })
+      .send(sleepEntry);
+    expect(res1.statusCode).toBe(201);
+    expect(res1.type).toBe("application/json");
+
+    // Add the second entry
+    const res2 = await request(server)
+      .post("/api/sleep")
+      .set({ authorization: token })
+      .send(sleepEntry2);
+    expect(res2.statusCode).toBe(201);
+    expect(res2.type).toBe("application/json");
+
+    // Add the third entry
+    const res3 = await request(server)
+      .post("/api/sleep")
+      .set({ authorization: token })
+      .send(sleepEntry3);
+    expect(res3.statusCode).toBe(201);
+    expect(res3.type).toBe("application/json");
+
+    // Get all data
+    const allSleep = await request(server)
+      .get("/api/sleep")
+      .set({ authorization: token });
+    expect(allSleep.statusCode).toBe(200);
+    expect(allSleep.type).toBe("application/json");
+    expect(allSleep.body.data).not.toHaveLength(0);
+    expect(allSleep.body.data).toHaveLength(3);
+    done();
+  });
+
+  it("returns only data for the logged in user", async done => {
+    expect(1).toBe(1);
+    done();
+  });
+
+  it("returns all data in a range when using a query string", async done => {
+    expect(1).toBe(1);
+    done();
+  });
+
+  // Update a sleep entry
   it("updates an existing sleep entry", async done => {
     try {
       // Register a user so that the update can change the user_id to this user id
@@ -181,6 +279,7 @@ describe("the sleep route", () => {
         .post("/api/register")
         .send(testAdmin);
       expect(regRes.statusCode).toBe(201);
+
       // Attempt login
       // prettier-ignore
       const loginRes = await request(server)
@@ -194,12 +293,14 @@ describe("the sleep route", () => {
       expect(token).toBeDefined();
       const validJWT = await isVerifiedJWT(token);
       expect(validJWT).toBe(true);
+
       // Add the entry
       const sleep = await request(server)
         .post("/api/sleep")
         .set({ authorization: token })
         .send(sleepEntry);
       const sleepInserted = sleep.body.data;
+
       // Update the entry
       const updatedEntry = {
         sleep_start: 1608549000001,
@@ -210,11 +311,22 @@ describe("the sleep route", () => {
         mood_day: 4,
         mood_bedtime: 4,
       };
+
+      // It should return a 401 if not logged in
+      // prettier-ignore
+      const noAuth = await request(server)
+        .put(`/api/sleep/${sleepInserted.id}`)
+        .send(updatedEntry)
+      expect(noAuth.statusCode).toBe(401);
+      expect(noAuth.type).toBe("application/json");
+      expect(noAuth.body).toEqual(authError);
+
       // prettier-ignore
       const res = await request(server)
         .put(`/api/sleep/${sleepInserted.id}`)
         .set({ authorization: token })
         .send(updatedEntry)
+
       // Test mood integration a little more rigourously
       console.log("updated Res", res.body);
       const updated = res.body.data;
